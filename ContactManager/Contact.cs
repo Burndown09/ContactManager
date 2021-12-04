64b2e0e0087d35ac8f00e4f191d654238d516256
Brandon Roberts <2032204@champlaincollege.qc.ca>
2021-12-04 13:09:07 -0500

    Add project files.

---
+++ ContactManager/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager
+{
+    class Contact
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+
+        public Contact(int ident, string fName, string lName, string pNumber, string elecMail)
+        {
+            this.id = ident;
+            this.firstName = fName;
+            this.lastName = lName;
+            this.phoneNumber = pNumber;
+            this.email = elecMail;
+        }
+        public Contact(string fName, string lName, string pNumber, string elecMail)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+            this.phoneNumber = pNumber;
+            this.email = elecMail;
+        }
+    }
+}
---
+++ ContactManager/Database.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace ContactManager
+{
+    class Database
+    {
+        public Database()
+        {
+            
+        }
+
+        public List<Contact> getContactList()
+        {
+            string sqltext = "select * from contacts";
+            var con = new SqlConnection("data source =.; database = contactmanager; integrated security = SSPI");
+            SqlCommand cmd = new SqlCommand(sqltext, con);
+
+            List<Contact> contactList = new List<Contact>();
+
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            while (sdr.Read())
+            {
+                Contact contact = new Contact((int)sdr["Id"], (string)sdr["first_name"], (string)sdr["last_name"], (string)sdr["phone_number"],
+                    (string)sdr["email"]);
+                
+                    contactList.Add(contact);
+            }
+            con.Close();
+
+            return contactList;
+        }
+
+        public void addContact(Contact cont)
+        {
+            string sqltext = "insert into contacts (first_name, last_name, phone_number, email) values(@fName, @lName, @pNumber, @eMail)";
+            var con = new SqlConnection("data source =.; database = contactmanager; integrated security = SSPI");
+            SqlCommand cmd = new SqlCommand(sqltext, con);
+            cmd.Parameters.AddWithValue("@fName", cont.firstName);
+            cmd.Parameters.AddWithValue("@lName", cont.lastName);
+            cmd.Parameters.AddWithValue("@pNumber", cont.phoneNumber);
+            cmd.Parameters.AddWithValue("@eMail", cont.email);
+            try {
+                con.Open();
+                var rowsAffected = cmd.ExecuteNonQuery();
+
+                string query = "Select @@Identity as newId from contacts";
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+                var newId = Convert.ToInt32(cmd.ExecuteScalar());
+
+                Console.WriteLine("Records Inserted Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally {
+                con.Close();
+            }
+        }
+
+        public void deleteContact(int ident)
+        {
+            string sqltext = "delete from contacts where id = @id";
+            var con = new SqlConnection("data source =.; database = contactmanager; integrated security = SSPI");
+            SqlCommand cmd = new SqlCommand(sqltext, con);
+            cmd.Parameters.AddWithValue("@id", ident);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        }
+    }
+}
